bbf60f4f4a80bd94228606882852e8853b1ca0df
ankit <ankitgci1@gamil.com>
2021-12-06 23:03:24 +0530

    Updates

---
+++ precache-manifest.1f27383be52cae990a22f7652e60e2bd.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0a128d3c9084dc1bc6fd8028060840a",
+    "url": "/metafox/index.html"
+  },
+  {
+    "revision": "5b1ceb8fec121fb111cf",
+    "url": "/metafox/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "422de2b6ebed9f3318a2",
+    "url": "/metafox/static/css/main.67e38394.chunk.css"
+  },
+  {
+    "revision": "5b1ceb8fec121fb111cf",
+    "url": "/metafox/static/js/2.6d9044bb.chunk.js"
+  },
+  {
+    "revision": "ba15f2fc257b12c969c1ba7f3c4ca9c9",
+    "url": "/metafox/static/js/2.6d9044bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7534d2dcbd09167fb9b9",
+    "url": "/metafox/static/js/3.a7d68c53.chunk.js"
+  },
+  {
+    "revision": "422de2b6ebed9f3318a2",
+    "url": "/metafox/static/js/main.1fa24c03.chunk.js"
+  },
+  {
+    "revision": "608410a95780729dcdf6",
+    "url": "/metafox/static/js/runtime-main.b57c3757.js"
+  },
+  {
+    "revision": "404c6e0258732ab49882404e57488382",
+    "url": "/metafox/static/media/bg.404c6e02.png"
+  },
+  {
+    "revision": "36c830257c2531288f3d69544bf2b37c",
+    "url": "/metafox/static/media/dash-bg.36c83025.png"
+  },
+  {
+    "revision": "6842be206e44ef044dec3413da8ed066",
+    "url": "/metafox/static/media/discord.6842be20.png"
+  },
+  {
+    "revision": "05d3f6a46f2723be32121c41b671c704",
+    "url": "/metafox/static/media/logo.05d3f6a4.png"
+  },
+  {
+    "revision": "b057f5f300cb89c57b78cbe06809670b",
+    "url": "/metafox/static/media/medium.b057f5f3.png"
+  },
+  {
+    "revision": "2002b07d247b5a078549af0703b569a1",
+    "url": "/metafox/static/media/twitter.2002b07d.png"
+  }
+]);
